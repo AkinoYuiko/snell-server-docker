3a3ee1db78598b8de994bc8961533c0804c1e050
Civi <civi@rcg.fun>
2024-04-23 20:39:49 +0800

    DSFSDLKJFHSJLKDFaa

--- download.sh
+++ download.sh
@@ -2,13 +2,13 @@
 
 case "$1" in
   "linux/amd64")
-    wget --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-amd64.zip" ;;
+    wget --dns-server=8.8.8.8 --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-amd64.zip" ;;
   "linux/arm64")
-    wget --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-aarch64.zip" ;;
+    wget --dns-server=8.8.8.8 --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-aarch64.zip" ;;
   "linux/arm/v7")
-    wget --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-armv7l.zip" ;;
+    wget --dns-server=8.8.8.8 --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-armv7l.zip" ;;
   "linux/386")
-    wget --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-i386.zip" ;;
+    wget --dns-server=8.8.8.8 --no-check-certificate -O snell.zip "https://dl.nssurge.com/snell/snell-server-v$2-linux-i386.zip" ;;
   *) echo "unsupported platform: $1"; exit 1 ;; 
   esac
 
