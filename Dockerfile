2ce2f33c690592684a44e241661e85ecb0751f56
Civi <civi@rcg.fun>
2025-06-30 14:14:43 +0800

    Rework from vocrx/snell-server

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,17 @@
-FROM --platform=${BUILDPLATFORM} debian AS builder
-
-WORKDIR /root
-
-ARG TARGETPLATFORM \
-    BUILDPLATFORM \
-    SNELL_SERVER_VERSION="5.0.0b1"
-
-COPY *.sh /root/
-
-RUN apt update && \
-    apt install unzip wget -y && \
-    bash ./download.sh ${TARGETPLATFORM} ${SNELL_SERVER_VERSION}
-
+# syntax=docker/dockerfile:1
 FROM debian:trixie-slim
 
-WORKDIR /root
-COPY --from=builder /root/snell-server /usr/bin/
-COPY entrypoint.sh /root/
-# Add Tini
-ADD https://github.com/krallin/tini/releases/download/v0.19.0/tini /tini
-RUN chmod +x /tini
+ARG TARGETPLATFORM
+ENV TARGETPLATFORM=${TARGETPLATFORM:-linux/amd64}
 
-ENV PORT=6250 \
-    IPV6=false \
-    DNS=1.1.1.1,1.0.0.1 \
-    PSK=
+COPY entrypoint.sh /snell/
 
-ENTRYPOINT ["/tini","--","/root/entrypoint.sh"]
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y wget unzip curl tini && \
+    chmod +x /snell/entrypoint.sh && \
+    rm -rf /var/lib/apt/lists/*
+
+WORKDIR /snell
+
+ENTRYPOINT ["/usr/bin/tini", "--", "/snell/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,58 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-BIN="snell-server"
-CONF="/root/snell-server.conf"
-
-run() {
-  if [ ! -f ${CONF} ]; then
-    PSK=${PSK:-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 31)}
-    PORT=${PORT:-6250}
-    IPV6=${IPV6:-false}
-    DNS=${DNS:-1.0.0.1}
-
-    echo "Using DNS: ${DNS}"
-    echo "Using PSK: ${PSK}"
-    echo "Using port: ${PORT}"
-    echo "Using ipv6: ${IPV6}"
-
-    echo "Generating new config..."
-    cat << EOF > ${CONF}
-[snell-server]
-listen = :::${PORT}
-psk = ${PSK}
-dns = ${DNS}
-ipv6 = ${IPV6}
-EOF
-  fi
-  echo -e "Starting snell-server...\n"
-  echo "Config:"
-  cat ${CONF}
-  ${BIN} -c ${CONF}
+random_port() {
+  shuf -i 1024-65535 -n 1
 }
 
-run
+random_psk() {
+  cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1
+}
+
+generate_config() {
+
+  PORT=${PORT:-$(random_port)}
+  PSK=${PSK:-$(random_psk)}
+  IPV6=${IPV6:-false}
+
+  cat >/snell/snell.conf <<EOF
+[snell-server]
+listen=:::$PORT
+psk=$PSK
+ipv6=$IPV6
+EOF
+
+  declare -A config_map=([DNS]="dns" [OBFS]="obfs" [HOST]="obfs-host")
+
+  for key in "${!config_map[@]}"; do
+    if [ -n "${!key}" ]; then
+      echo "${config_map[$key]}=${!key}" >>/snell/snell.conf
+    fi
+  done
+}
+
+download_snell() {
+  VERSION=${VERSION:-v4.1.1}
+  case "${TARGETPLATFORM}" in
+    "linux/amd64") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-amd64.zip" ;;
+    "linux/386") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-i386.zip" ;;
+    "linux/arm64") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-aarch64.zip" ;;
+    "linux/arm/v7") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-armv7l.zip" ;;
+    *) echo "不支持的平台: ${TARGETPLATFORM}" && exit 1 ;;
+    esac
+
+  wget -q -O snell.zip ${SNELL_URL} &&
+    unzip -qo snell.zip -d /snell &&
+    rm snell.zip &&
+    chmod +x /snell/snell-server
+}
+
+download_snell
+generate_config
+echo "PORT:$PORT"
+echo "PSK:$PSK"
+echo "VERSION:$VERSION"
+[ -n "$DNS" ] && echo "DNS:$DNS"
+[ -n "$OBFS" ] && echo "OBFS:$OBFS"
+[ -n "$HOST" ] && echo "HOST:$HOST"
+exec /snell/snell-server -c /snell/snell.conf -l ${LOG:-notify}
